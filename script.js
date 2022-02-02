d8010ca49355a35393c4662495967c28709c9cc0
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-02-02 09:05:51 +0000

    Third upload
    part of filtering

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+const skillSet = document.querySelectorAll(".skillSet");
+const filterBox = document.querySelector('.filter-box')
+const filter = []
+const clearData = document.querySelector(".Clear")
 
+const showFilters = skills => {
+    skills.forEach(el => {
+        filterBox.append(el)
+    })
+}
 
+skillSet.forEach(el => {
+    el.addEventListener('click', ev => {
+        filter.push(ev.target.innerHTML)
+        console.log(filter)
+        filterBox.innerHTML = ""
+        filterBox.append(filter)
+    })
+})
+
+clearData.addEventListener('click', (e) => {
+    filterBox.innerHTML = "";
+})
